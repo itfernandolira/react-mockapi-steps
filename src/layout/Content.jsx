10d5f23fc1b9773408516c89dd43790c67e0bb42
Fernando Lira <it.fernandolira@gmail.com>
2022-06-08 15:17:36 +0100

    Movie content - fetch

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
 import Movies from "./Movies";
+import Movie from "./Movie";
 
 const Content= props => {
     return (
     <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/:category" element={<Movies />} />
+        <Route path="/:category/movies/:movie" element={<Movie />} />
     </Routes>
     )
 }
---
+++ src/layout/Movie.jsx
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from "react";
+import { useParams } from 'react-router-dom'
+
+const Movie = props => {
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [movies, setMovies] = useState([]);
+
+    var { category, movie } = useParams();
+
+    useEffect(() => {
+        fetch("https://629a16cd7b866a90ec4954ee.mockapi.io/api/v1/categories/"+category+"/movies/"+movie)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setMovies([data]);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    },[]);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <>
+                {movies.map(movie => (
+                    <div className="card mb-3" key={movie.id}>
+                        <img src={movie.avatar} className="card-img-top" alt={movie.name} />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.name}</h5>
+                                <p className="card-text">{ movie.info + "..." }</p>
+                                <p className="card-text"><small className="text-muted">Created at {movie.createdAt.slice(0,10)}</small></p>
+                            </div>
+                    </div>
+                ))}
+                
+            </>
+        );
+    }
+
+}
+
+export default Movie
